740800079806905d58854189decf18673ac74bbc
catalinaIlabaca <catalina.a.ilabaca@uchile.cl>
2025-05-09 15:33:38 -0400

    chore: updates test.sh to generate coverage badge

--- .github/test.sh
+++ .github/test.sh
@@ -1,13 +1,19 @@
 #!/bin/dash
-pip install -e git+https://github.com/eol-uchile/uchileedxlogin@8cb702fe18c26b29c0667c660c24ee75a03c9ec9#egg=uchileedxlogin
-pip install -e /openedx/requirements/eol_report_certificate
+set -e
 
-cd /openedx/requirements/eol_report_certificate/eolreportcertificate
-cp /openedx/edx-platform/setup.cfg .
+pip install --src /openedx/venv/src -e git+https://github.com/eol-uchile/uchileedxlogin@0.0.1#egg=uchileedxlogin
+pip install --src /openedx/venv/src -e /openedx/requirements/app
+pip install pytest-cov genbadge[coverage]
+
+cd /openedx/requirements/app
+
 mkdir test_root
-cd test_root/
-ln -s /openedx/staticfiles .
+ln -s /openedx/staticfiles ./test_root/
 
-cd /openedx/requirements/eol_report_certificate/eolreportcertificate
+cd /openedx/requirements/app
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest eolreportcertificate/tests.py
+
+rm -rf test_root
+
+genbadge coverage
